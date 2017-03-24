2774b66b7b2420447fae2b864d14e14ec3abcc71
Mesi Rendon <amrendonsa@unal.edu.co>
2017-03-24 10:36:55 -0500

    Adding request specs and product controller
    I've added the request specs for products as well as its controller
    handling every possible request and scenarios using rspec
    
    https://trello.com/c/TQYLi8Wv/

--- spec/requests/products_spec.rb
+++ spec/requests/products_spec.rb
@@ -8,7 +8,7 @@ RSpec.describe 'Products API', type: :request do
     before { get '/products' }
 
     it 'returns products' do
-      expect(json).not_to must_be_empty
+      expect(json).not_to be_empty
       expect(json.size).to eq(10)
     end
 
@@ -22,7 +22,7 @@ RSpec.describe 'Products API', type: :request do
 
     context 'when the record exists' do
       it 'returns the product' do
-        expect(json).not_to must_be_empty
+        expect(json).not_to be_empty
         expect(json['id']).to eq(product_id)
       end
 
@@ -37,6 +37,62 @@ RSpec.describe 'Products API', type: :request do
       it 'returns status code 404' do
         expect(response).to have_http_status(404)
       end
+
+      it 'returns a not found message' do
+        expect(response.body).to match(/Couldn't find Product/)
+      end
+    end
+  end
+
+  describe 'POST /product' do
+    let(:valid_attributes) { {name: 'Guinness'} }
+
+    context 'when the request is valid' do
+      before { post '/products', params: valid_attributes }
+
+      it 'creates a product' do
+        expect(json['name']).to eq('Guinness')
+      end
+
+      it 'returns status code 201' do
+        expect(response).to have_http_status(201)
+      end
+    end
+
+    context 'when the request is invalid' do
+      before { post '/products', params: {naem: "Guinness"} }
+
+      it 'returns status code 422' do
+        expect(response).to have_http_status(422)
+      end
+
+      it 'returns a validation failure message' do
+        expect(response.body).to match(/Validation failed: Name can't be blank/)
+      end
+    end
+  end
+
+  describe 'PUT /products/:id' do
+    let(:valid_attributes) { {name: 'Guinness'} }
+
+    context 'when the record exists' do
+      before { put "/products/#{product_id}", params: valid_attributes }
+
+      it 'updates the record' do
+        expect(response.body).to be_empty
+      end
+
+      it 'returns status code 204' do
+        expect(response).to have_http_status(204)
+      end
+    end
+  end
+
+  describe 'DELETE /products/:id' do
+    before { delete "/products/#{product_id}" }
+
+    it 'returns status code 204' do
+      expect(response).to have_http_status(204)
     end
   end
 end
