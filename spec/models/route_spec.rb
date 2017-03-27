d386ab73c50a0188476fe265afee47d11e83b890
Mesi Rendon <amrendonsa@unal.edu.co>
2017-03-26 22:58:38 -0500

    Fixing model and request specs for `Routes`
    There was a problem with the way rspec demands this type of associations
    must be done and also in the way the migrations were done in the test
    database.
    
    I also realized there is no need to execute `bundle exec rspec`,
    executing `rspec` is enough.
    
    https://trello.com/c/8APDidfs/

--- spec/models/route_spec.rb
+++ spec/models/route_spec.rb
@@ -2,4 +2,7 @@ require 'rails_helper'
 
 RSpec.describe Route, type: :model do
   # Ensure Route has relations to Places for both Origin and Destination
+
+  it { should belong_to(:origin) }
+  it { should belong_to(:destination) }
 end
