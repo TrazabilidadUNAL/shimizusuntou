ddf908e976040cd74878440f042f53496544e9e9
Mesi Rendon <amrendonsa@unal.edu.co>
2017-04-07 08:36:28 -0500

    Fix on public views for api
    Currently there is an error 500 when accessing to invalid routes through
    the website in heroku. This fix that error.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,4 @@ class ApplicationController < ActionController::API
   include ActionController::Serialization
   include Response
   include ExceptionHandler
-
-  def not_found
-    format.all { head :not_found }
-  end
 end
