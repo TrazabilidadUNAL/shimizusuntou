b2203ecd995c56597ca85559abe9d78bc86b9b92
Mesi Rendon <amrendonsa@unal.edu.co>
2017-04-20 08:58:36 -0500

    Finishing `Product` documentation
    I've fixed some glitches in the `Product` controller and finished with
    the `Product` documentation.
    
    https://trello.com/c/ksEPBIEV

--- app/controllers/api/v1/products_controller.rb
+++ app/controllers/api/v1/products_controller.rb
@@ -17,7 +17,7 @@ module Api::V1
     end
 
     api! 'Shows all products'
-    description 'Retrieves the whole list of products in the system'
+    description 'Retrieves the whole list of products in the system. If there are not enough records an empty array is returned.'
     formats ['json']
     example <<-EOM
     HTTP/1.1 200 OK
@@ -48,6 +48,7 @@ module Api::V1
         ]
     }
     EOM
+
     def index
       @products = Product.where(show: true)
       json_response(@products)
@@ -56,6 +57,7 @@ module Api::V1
     api! 'Shows an specific product'
     description 'Retrieves a single product in the system'
     formats ['json']
+    error :code => 404, :desc => "Product with that id hasn't been found"
     example <<-EOM
     HTTP/1.1 200 OK
     Cache-Control: max-age=0, private, must-revalidate
@@ -75,13 +77,19 @@ module Api::V1
         }
     }
     EOM
+
     def show
-      json_response(@product)
+      if @product
+        json_response(@product)
+      else
+        head :not_found
+      end
     end
 
     api! 'Creates a new product'
+    description 'Creates a new product having its name as a parameter.'
     param_group :product
-    error :code => 500, :desc => 'Internal server error', :meta => {:problem => 'Missing `name` parameter'}
+    error :code => 422, :desc => 'Unprocessable entity', :meta => {:problem => 'Missing `name` parameter'}
     example <<-EOM
     HTTP/1.1 201 Created
     Cache-Control: max-age=0, private, must-revalidate
@@ -102,34 +110,69 @@ module Api::V1
         }
     }
     EOM
+
     def create
       @product = Product.create!(product_params)
       json_response(@product, :created)
     end
 
-    # PUT /products/:id
-    api!
+    api! 'Updates an existing product'
+    description 'Updates an existing product.'
+    param_group :product
+    error :code => 404, :desc => "Product with that id hasn't been found."
+    example <<-EOM
+    HTTP/1.1 204 No Content
+    Cache-Control: no-cache
+    Vary: Origin
+    X-Content-Type-Options: nosniff
+    X-Frame-Options: SAMEORIGIN
+    X-Request-Id: 5088731b-2133-4552-9aeb-bb35edebbcee
+    X-Runtime: 0.024239
+    X-XSS-Protection: 1; mode=block
+    EOM
+
     def update
-      @product.update(product_params)
-      head :no_content
+      if @product
+        @product.update(product_params)
+        head :no_content
+      else
+        head :not_found
+      end
     end
 
     # DELETE /products/:id
-    api!
+    api! 'Deletes a product in the system'
+    description 'Deletes a product in the system given an id.'
+    error :code => 404, :desc => "Product with that id hasn't been found."
+    example <<-EOM
+    HTTP/1.1 204 No Content
+    Cache-Control: no-cache
+    Vary: Origin
+    X-Content-Type-Options: nosniff
+    X-Frame-Options: SAMEORIGIN
+    X-Request-Id: 841effba-dbac-4dae-a52c-312d5cdc4d5f
+    X-Runtime: 0.021002
+    X-XSS-Protection: 1; mode=block
+    EOM
+
     def destroy
-      @product.show = false
-      @product.save!
-      head :no_content
+      if @product
+        @product.show = false
+        @product.save!
+        head :no_content
+      else
+        head :not_found
+      end
     end
 
     private
 
     def product_params
-      params.permit(:name)
+      permitted = params.permit(:name)
     end
 
     def set_product
-      @product = Product.where(show: true).find(params[:id])
+      @product = Product.exists?(params[:id]) ? Product.where(show: true).find(params[:id]) : nil
     end
   end
 end
