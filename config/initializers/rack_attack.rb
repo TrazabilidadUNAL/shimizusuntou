bccc4b9aabfa27fd09381e34f324076e01e36fc1
Mesi Rendon <amrendonsa@unal.edu.co>
2017-05-30 09:33:55 -0500

    Rise rack attack limits

--- config/initializers/rack_attack.rb
+++ config/initializers/rack_attack.rb
@@ -10,7 +10,7 @@ class Rack::Attack
   end
 
   # Allow an IP address to make 5 requests every 5 seconds
-  throttle('req/ip', limit: 5, period: 5.seconds) do |req|
+  throttle('req/ip', limit: 10, period: 1.seconds) do |req|
     req.ip
   end
 
